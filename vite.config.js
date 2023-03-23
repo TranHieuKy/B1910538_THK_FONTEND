633ecebbd866948b2ef694c71b2717555de47313
Tran Hieu Ky <b1910538@student.ctu.edu.vn>
2023-03-23 22:44:18 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,17 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-    
-    
+
+
   },
   server: {
-    port: 3001,
-    },
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+
+      },
+    }
+  },
 });
